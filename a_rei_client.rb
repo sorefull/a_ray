00e346ef5542a0dfe726746de7ad12641e37adef
Oleg Cherednichenko <oleg.cherednichenko@wimdu.com>
2017-08-25 16:41:07 +0300

    refactor needed

--- a_rei_client.rb
+++ a_rei_client.rb
@@ -1,10 +1,19 @@
 #!/usr/bin/env ruby -w
 require "socket"
+require 'encrypto_signo'
+
 class AReiClient
+  CODE = 'secret'
+
   def initialize(server)
     @server = server
     @request = nil
     @response = nil
+
+    keypair = EncryptoSigno.generate_keypair
+    @private_key = keypair.to_s
+    @public_key = keypair.public_key.to_s
+
     listen
     send
     @request.join
@@ -14,20 +23,35 @@ class AReiClient
   def listen
     @response = Thread.new do
       loop {
-        msg = @server.gets.chomp
+        msg = decrypt(@server.gets.chomp.gsub(CODE, "\n"))
         puts "#{msg}"
       }
     end
   end
 
   def send
+    @server_public_key = @server.gets.chomp.gsub(CODE, "\n")
+    @server.puts encrypt(@public_key).gsub("\n", CODE)
+
     puts "Enter the username:"
     @request = Thread.new do
       loop {
         msg = $stdin.gets.chomp
-        @server.puts(msg)
+        next if msg == ''
+        @server.puts encrypt(msg).gsub("\n", CODE)
+        exit! if msg == 'exit!'
       }
     end
+  end
+
+  private
+
+  def encrypt(string)
+    EncryptoSigno.encrypt(@server_public_key, string)
+  end
+
+  def decrypt(encrypted_string)
+    EncryptoSigno.decrypt(@private_key, encrypted_string)
   end
 end
 
--- a_rei_server.rb
+++ a_rei_server.rb
@@ -1,45 +1,72 @@
 #!/usr/bin/env ruby -w
 require "socket"
+require 'encrypto_signo'
+
+class TCPSocket
+  attr_accessor :key
+end
 
 class AReiServer
+  CODE = 'secret'
+
   def initialize(port, ip)
     @server = TCPServer.open(ip, port)
-    @connections = Hash.new
-    @rooms = Hash.new
-    @clients = Hash.new
+    @connections = {}
     @connections[:server] = @server
-    @connections[:rooms] = @rooms
-    @connections[:clients] = @clients
+    @connections[:clients] = {}
+
+    keypair = EncryptoSigno.generate_keypair
+    @private_key = keypair.to_s
+    @public_key = keypair.public_key.to_s
+
     run
   end
 
   def run
     loop {
-      Thread.start(@server.accept) do | client |
-        nick_name = client.gets.chomp.to_sym
+      Thread.start(@server.accept) do |client|
+        client.puts @public_key.gsub("\n", CODE)
+        client.key = decrypt(client.gets.chomp.gsub(CODE, "\n"))
+
+        nick_name = decrypt(client.gets.chomp.gsub(CODE, "\n")).to_sym
         @connections[:clients].each do |other_name, other_client|
           if nick_name == other_name || client == other_client
-            client.puts "This username already exist"
+            client.puts encrypt('This username already exist', client.key).gsub("\n", CODE)
             Thread.kill self
           end
         end
-        puts "#{nick_name} #{client}"
+        puts "Online: #{nick_name} - #{client}"
         @connections[:clients][nick_name] = client
-        client.puts "Connection established, Thank you for joining! Happy chatting"
-        listen_user_messages( nick_name, client )
+        client.puts encrypt("Connection established, Thank you for joining! Happy chatting", client.key).gsub("\n", CODE)
+        listen_user_messages(nick_name, client)
       end
     }.join
   end
 
   def listen_user_messages(username, client)
     loop {
-      msg = client.gets.chomp
+      msg = decrypt(client.gets.chomp.gsub(CODE, "\n"))
+      if msg == 'exit!'
+        @connections[:clients].delete(username)
+        puts "Offline: #{username} - #{client}"
+        next
+      end
       @connections[:clients].each do |other_name, other_client|
         unless other_name == username
-          other_client.puts "#{username.to_s}: #{msg}"
+          other_client.puts encrypt("#{username.to_s}: #{msg}", other_client.key).gsub("\n", CODE)
         end
       end
     }
+  end
+
+  private
+
+  def encrypt(string, client_public_key)
+    EncryptoSigno.encrypt(client_public_key, string)
+  end
+
+  def decrypt(encrypted_string)
+    EncryptoSigno.decrypt(@private_key, encrypted_string)
   end
 end
 
